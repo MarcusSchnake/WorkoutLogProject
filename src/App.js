272b12a1a7631b1bb5fba9676ca59636cd46bdbe
Marcus Schnake <marcusschnake@Marcuss-MBP.lan>
2021-11-18 21:34:58 -0500

    workoutlog  client first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import SiteBar from './home/Navbar';
+import Auth from './auth/Auth';
+import WorkoutIndex from './workouts/WorkoutIndex';
+
 
 function App() {
+
+  const [sessionToken, setSessionToken] = useState('');// setting up token so it can be used by child components this and useEffect
+
+  useEffect(() => {// useEffect is a hook that runs after the first render
+    if(localStorage.getItem('token')){// if token is in local storage
+      setSessionToken(localStorage.getItem('token'));// set session token to token in local storage
+    }
+  }, []);
+
+  const updateToken = (newToken) => {// allows child components to update the token
+    localStorage.setItem('token', newToken);// set token in local storage to new token
+    setSessionToken(newToken);// set session token to new token
+    console.log(sessionToken);// log session token
+  }
+
+  const clearToken = () => {// allows child components to clear the token
+    localStorage.clear();// clear token in local storage
+    setSessionToken('');// set session token to empty string
+  }
+
+  const protectedViews = () => {// protected views
+    return (sessionToken === localStorage.getItem('token') ? <WorkoutIndex token={sessionToken}/>// if session token is equal to token in local storage, return workout index
+    : <Auth updateToken={updateToken} />) // else return auth
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SiteBar clickLogout={clearToken}/>
+      {protectedViews()}
     </div>
   );
 }
